0aa225143a39c7902195b647284e06c9e0a46a4f
zihan_mac_air <32484940+wzh4464@users.noreply.github.com>
2025-06-28 21:09:24 +0800

    2099. Find Subsequence of Length K With the Largest Sum

--- main.rs
+++ main.rs
@@ -3,7 +3,7 @@
  * Created Date: Tuesday, June 24th 2025
  * Author: Zihan
  * -----
- * Last Modified: Thursday, 26th June 2025 4:19:44 pm
+ * Last Modified: Saturday, 28th June 2025 9:08:48 pm
  * Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  * -----
  * HISTORY:
@@ -14,19 +14,27 @@
 struct Solution;
 
 impl Solution {
-    pub fn divide_string(s: String, k: i32, fill: char) -> Vec<String> {
-        let batch_num = (s.len() - 1) / k as usize + 1;
-        let mut result = vec![String::new(); batch_num as usize];
-        for i in 0..batch_num {
-            result[i] = s[(i * k as usize)..((i + 1) * k as usize).min(s.len())].to_string();
-        }
-        let last_len = result.last().unwrap().len();
-        if last_len < k as usize {
-            for _ in last_len..k as usize {
-                result.last_mut().unwrap().push(fill);
+    pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut sorted_with_idx = nums
+            .iter()
+            .take(k as usize)
+            .cloned()
+            .enumerate()
+            .collect::<Vec<_>>();
+        sorted_with_idx.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+
+        for (i, &num) in nums.iter().skip(k as usize).enumerate() {
+            if let Some(&min) = sorted_with_idx.last() {
+                let i = i + k as usize;
+                if num > min.1 {
+                    sorted_with_idx.pop();
+                    let idx = sorted_with_idx.partition_point(|&x| x.1 > num);
+                    sorted_with_idx.insert(idx, (i, num));
+                }
             }
         }
-        result
+        sorted_with_idx.sort_by_key(|&x| x.0);
+        sorted_with_idx.iter().map(|v| v.1).collect()
     }
 }
 
@@ -35,18 +43,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_divide_string() {
-        let s = "abcdefghi".to_string();
-        let k = 3;
-        let fill = 'x';
-        let result = Solution::divide_string(s, k, fill);
-        assert_eq!(result, vec!["abc", "def", "ghi"]);
-        
-        let s2 = "abcdefghij".to_string();
+    fn test_max_subsequence_1() {
+        let nums = vec![2, 1, 3, 3];
+        let k = 2;
+        let result = Solution::max_subsequence(nums, k);
+        // 测试将在实现完成后进行验证
+        assert_eq!(result, vec![3, 3]);
+    }
+
+    #[test]
+    fn test_max_subsequence_2() {
+        let nums2 = vec![-1, -2, 3, 4];
         let k2 = 3;
-        let fill2 = 'x';
-        let result2 = Solution::divide_string(s2, k2, fill2);
-        assert_eq!(result2, vec!["abc", "def", "ghi", "jxx"]);
+        let result2 = Solution::max_subsequence(nums2, k2);
+        assert_eq!(result2, vec![-1, 3, 4]);
     }
 }
 
